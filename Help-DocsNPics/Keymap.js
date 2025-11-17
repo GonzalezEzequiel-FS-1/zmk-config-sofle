16c05ad2b74ab380a07d9bad62b88cc5936faafb
Zeke <148794920+GonzalezEzequiel-FS-1@users.noreply.github.com>
2025-11-17 14:02:02 -0500

    Almost done with documentation

---
+++ Help-DocsNPics/Keymap.js
@@ -0,0 +1,17 @@
+const keymap = {
+  columns: {
+    c0: "gpio0 006",
+    c1: "gpio0 008",
+    c2: "gpio0 017",
+    c3: "gpio0 020",
+    c4: "gpio0 022",
+    c5: "gpio0 024",
+  },
+  rows: {
+    r0: "gpio0 011",
+    r1: "gpio0 031",
+    r2: "gpio0 029",
+    r3: "gpio0 002",
+    r4: "gpio0 010",
+  },
+};
